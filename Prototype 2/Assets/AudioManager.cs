b569329c232a786c36a253d57e80145bee706db9
Devin Thorpe <devinthorpe77@gmail.com>
2025-02-03 22:50:15 -0800

    Score, music, SFX

---
+++ Prototype 2/Assets/AudioManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    // sound effects
+    public AudioClip death;
+    public AudioClip jump;
+    public AudioClip background1;
+    public AudioClip background2;
+    public AudioClip background3;
+    public AudioClip background4;
+}
--- Prototype 2/Assets/Ground.cs
+++ Prototype 2/Assets/Ground.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using TMPro;
 
 public class Ground : MonoBehaviour
 {
     private MeshRenderer meshRenderer;
-
+    private float distanceTraveled = 0f;
+    [SerializeField] private TMP_Text scoreText; // Assign in Inspector Score Text
+    
+    void Start()
+    {
+        UpdateScoreText();
+    }
     private void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
@@ -13,5 +20,21 @@ public class Ground : MonoBehaviour
     {
         float speed = GameManager.Instance.gameSpeed / transform.localScale.x;
         meshRenderer.material.mainTextureOffset += Vector2.right * speed * Time.deltaTime;
+
+        distanceTraveled += speed * Time.deltaTime; //calc distance of player
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = distanceTraveled.ToString("F3");
+        }
+        else
+        {
+            Debug.LogWarning("Score Text is not assigned in ScoreManager! Assign it in the Inspector.");
+        }
     }
 }
+
